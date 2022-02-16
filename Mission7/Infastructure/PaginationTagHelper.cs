92aac9038a57bce4f9ddeda6f5797260ff631783
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-02-16 09:50:00 -0700

    more

--- Mission7/Infastructure/PaginationTagHelper.cs
+++ Mission7/Infastructure/PaginationTagHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -29,14 +30,34 @@ namespace Mission7.Infastructure
         public PageInfo BookPage { get; set; } //this has to be named the same as the Attributes thing up above.
         public string PageAction { get; set; }
 
+        //this is the styles for the stuff
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
             TagBuilder final = new TagBuilder("div");
-            for (int i = 1; i < BookPage.TotalPages + 1; i++)
+            for (int i = 1; i <= BookPage.TotalPages; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                //this code below adds the css classes to the a tag
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    if(i == BookPage.CurrentPage)
+                    {
+                        tb.AddCssClass(PageClassSelected);
+                    }
+                    else
+                    {
+                        tb.AddCssClass(PageClassNormal);
+                    }
+                }
+                //code above adds the css classes from bootstrap
                 tb.InnerHtml.Append(i.ToString());
                 final.InnerHtml.AppendHtml(tb);
             }
