99c47ff308790012348a093f2e98fd0cd025675b
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-03-09 09:57:51 -0700

    mission 10

--- Mission7/Models/EFBookstoreRepository.cs
+++ Mission7/Models/EFBookstoreRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 
+//this file is the implementation of the IBookStorerepository 
 namespace Mission7.Models
 {
     public class EFBookstoreRepository : IBookstoreRepository
@@ -11,5 +12,22 @@ namespace Mission7.Models
         public EFBookstoreRepository(BookstoreContext temp) => context = temp;
 
         public IQueryable<Book> Books => context.Books;
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
     }
 }
